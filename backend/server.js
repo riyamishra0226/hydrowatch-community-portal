67a28a072c276430041ae2b8096e7dcd65fe2eba
riyamishra0226 <riyamishra0226@gmail.com>
2026-07-11 15:16:44 +0530

    Fix API base URL

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,12 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://hydrowatch-community-portal.vercel.app",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
